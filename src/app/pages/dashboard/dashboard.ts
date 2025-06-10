68c8457ee1b1aa00bde7922a37aa110983354378
Luis Aguilar <luisaguilar_92@hotmail.com>
2025-06-10 17:44:31 -0600

    Add side-bar component and integrate with dashboard

--- src/app/pages/dashboard/dashboard.ts
+++ src/app/pages/dashboard/dashboard.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { SideBar } from '../../side-bar/side-bar';
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, SideBar],
   templateUrl: './dashboard.html',
   styleUrl: './dashboard.scss'
 })
